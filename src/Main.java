09916f250d7f8fd80c627885cdee4bdc3763a4ed
fvaquero <fernando.vaquero@a.vedrunasevillasj.es>
2024-03-01 15:41:41 +0100

    Tercer commit, añado nu bucle

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Adios");
+        for (int i= 0; i < 10; i++) {
+            System.out.println(i);
+        }
 
     }
 }
